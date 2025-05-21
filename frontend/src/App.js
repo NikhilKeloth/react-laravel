f4195a4304dc8011f3ba07ff589248c7a3600cce
Nikhil <nikhilkkalliad@gmail.com>
2025-05-21 20:36:20 +0530

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import NameForm from './components/NameForm';
-import './App.css';
+// import React from 'react';
+// import NameForm from './components/NameForm';
+// import './App.css';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>
+//       <NameForm />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LoadingPage from './common/LoadingPage';
+import AdminLogin from './admin/pages/Login';
+// import VendorLogin from './vendor/pages/Login';
+// import AdminDashboard from './admin/pages/Dashboard';
+// import VendorDashboard from './vendor/pages/Dashboard';
+
+function App() {
   return (
-    <div>
-      <NameForm />
-    </div>
+    <Router>
+      <Routes>
+        {/* Common */}
+        <Route path="/" element={<LoadingPage />} />
+    
+        {/* Admin Routes */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+        
+       
+      </Routes>
+    </Router>
   );
-};
+}
+//  {/* Admin Routes */}
+//         <Route path="/admin/login" element={<AdminLogin />} />
+//         <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+//         {/* Vendor Routes */}
+//         <Route path="/vendor/login" element={<VendorLogin />} />
+//         <Route path="/vendor/dashboard" element={<VendorDashboard />} />
 
 export default App;
---
+++ frontend/src/admin/pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import Counter from '../components/Counter';
+
+const Home = () => {
+    return (
+        <div>
+            <h1> here </h1>
+            
+        </div>
+    );
+};
+
+export default Home;
